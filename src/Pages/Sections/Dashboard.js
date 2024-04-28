2b1e44b5efb73eec54761589956ee1d55cba3a00
Ordinateur <mds.skali@gmail.com>
2024-04-28 13:44:22 +0100

    dashtest

--- src/Pages/Sections/Dashboard.js
+++ src/Pages/Sections/Dashboard.js
@@ -7,8 +7,8 @@ const Dashboard = () => {
         <div className="">
             <Nav />
             <div className="h-[100vh] w-full flex flex-col justify-center text-center bg-cover    " style={{ backgroundImage: `url(${back})`, backgroundPosition: 'center right' }}>
-                <h1 className='font-Lostar text-[131.05px] text-white  mt-[13rem]' >AYL AGENCY</h1>
-                <p className=' font-poppins text-[15px] text-white mt-[-2rem] ' >Purpose-Driven growth</p>
+                <h1 className='font-Lostar xl:text-[131.05px] text-white  mt-[13rem] text-[72px]' >AYL AGENCY</h1>
+                <p className=' font-poppins text-[15px] text-white xl:mt-[-2rem] mt-[0rem] ' >Purpose-Driven growth</p>
                 <div className=" flex justify-center items-center p-5 ">
                     <a href="/#Services" className=' font-poppins  text-[20.39px] text-[#FFF500]  ' >Services</a>
                     <img src={left} className=' h-auto w-[10.55px] ml-2 ' alt="" />
--- src/Pages/Sections/Nav.js
+++ src/Pages/Sections/Nav.js
@@ -19,12 +19,12 @@ const Nav = () => {
         <div className="">
             <div className="w-full h-[50px] bg-white shadow-md flex justify-between px-2  z-30  xl:h-[70px]">
                 <a href="/" style={{ textDecoration: 'none' }} className=' items-center h-full '  >
-                    <p className='font-Lostar text-[33px] text-[#A51D21] items-center justify-center m-auto pt-3' >Ayl agency</p>
+                    <p className='font-Lostar xl:text-[33px] text-[22px] text-[#A51D21] items-center justify-center m-auto pt-3' >Ayl agency</p>
                 </a>
                 <div className={'flex justify-center flex-col w-[17px] burger xl:hidden'} onClick={handleClickOnNav}>
-                    <div className="w-[10px] h-1 bg-[#559FBB] rounded-xl"></div>
-                    <div className="w-[17px] h-1 bg-[#559FBB] rounded-xl mt-[3px] mb-[3px]"></div>
-                    <div className="w-[10px] ml-auto mr-0 h-1 bg-[#559FBB] rounded-xl"></div>
+                    <div className="w-[10px] h-1 bg-[#A51D21] rounded-xl"></div>
+                    <div className="w-[17px] h-1 bg-[#A51D21] rounded-xl mt-[3px] mb-[3px]"></div>
+                    <div className="w-[10px] ml-auto mr-0 h-1 bg-[#A51D21] rounded-xl"></div>
                 </div>
                 <div className=" hidden xl:flex xl:items-center xl:justify-center xl:mr-6 ">
                     <a id='NavContact' href="/#Info" className=' font-Jaldi mx-2 text-[#000] ' >About us</a>
@@ -36,22 +36,11 @@ const Nav = () => {
             <div className={isActive ? 'sidenav w-full h-full bg-white absolute z-30 top-0' : 'hidden sidenav w-full h-full bg-white  top-0'} >
                 <img className="h-[15px] w-[15px]  m-auto relative top-5 " onClick={handleClickOnNav} src={del} alt="Close" />
                 <div className="flex flex-col justify-center items-center h-full">
-                    <a href="/" className="font-popins text-[15px] text-[#012A5B] border-b w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>Home</a>
-                    <a id='NavContact' href="/#contact" className="font-popins text-[15px] text-[#012A5B] border-b w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>Contact us</a>
-                    <a id='NavAbout' href="/#about" className="font-popins text-[15px] text-[#012A5B] border-b w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>About us</a>
-                    <a id='Navteritory' href="/#territory" className="font-popins text-[15px] text-[#012A5B] w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>Covered territory</a>
-                    <a href="/#services" className="font-popins text-[15px] text-[#012A5B] w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>services</a>
-                    <div className=" flex w-[80%]  mt-4 justify-center ">
-                        <div className={selectedlang === "eng" ? 'hidden' : 'w-[60px] h-[35px] rounded-sm bg-[#012A5B] text-white flex justify-center items-center font-Jaldi mr-2'}>
-                            <p id="btnlang" className=' cursor-pointer ' >eng</p>
-                        </div>
-                        <div className={selectedlang === "fr" ? 'hidden' : 'w-[60px] h-[35px] rounded-sm bg-[#012A5B] text-white flex justify-center items-center font-Jaldi mr-2'}>
-                            <p id="btnlang" className=' cursor-pointer ' >fr</p>
-                        </div>
-                        <div className={selectedlang === "esp" ? 'hidden' : 'w-[60px] h-[35px] rounded-sm bg-[#012A5B] text-white flex justify-center items-center font-Jaldi mr-2'}>
-                            <p id="btnlang" className=' cursor-pointer ' >esp</p>
-                        </div>
-                    </div>
+                    <a href="/#Info" className="font-popins text-[15px] text-[#000] border-b w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>About us</a>
+                    <a id='NavContact' href="/#Services" className="font-popins text-[15px] text-[#000] border-b w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>Services</a>
+                    <a id='Navteritory' href="/#Contact" className="font-popins text-[15px] text-[#000] w-[80%] border-b text-center pl-2 block mt-4" onClick={handleClickOnNav}>Contact us</a>
+                    <a href="/#Reviews" className="font-popins text-[15px] text-[#000] w-[80%] text-center pl-2 block mt-4" onClick={handleClickOnNav}>Reviews</a>
+                   
                 </div>
             </div>
 
